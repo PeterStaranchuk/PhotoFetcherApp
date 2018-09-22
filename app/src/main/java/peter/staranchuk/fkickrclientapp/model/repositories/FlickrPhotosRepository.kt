9862a93f6ccf58d84d059765dee03a4a373d9a63
peterstaranchuk <petr.staranchuk@gmail.com>
2018-09-22 14:17:56 +0300

    Add repository to request photos from Flickr api Add new settings for Flickr data source: number of photo per page, key for medium sized image

---
+++ app/src/main/java/peter/staranchuk/fkickrclientapp/model/repositories/FlickrPhotosRepository.kt
@@ -0,0 +1,27 @@
+package peter.staranchuk.fkickrclientapp.model.repositories
+
+import io.reactivex.Single
+import peter.staranchuk.fkickrclientapp.model.objects.FlickrImageUrlRetriever
+import peter.staranchuk.fkickrclientapp.model.objects.FlickrPhoto
+import peter.staranchuk.fkickrclientapp.network.FlickrApi
+import peter.staranchuk.fkickrclientapp.network.sources_settings.FlickrSettings
+import javax.inject.Inject
+
+class FlickrPhotosRepository @Inject constructor(private val flickrApi: FlickrApi, private val settings: FlickrSettings) {
+
+    fun getPhotos(pageNumber: Int): Single<List<FlickrPhoto>> {
+        return flickrApi.getRecentPhotos(
+                itemsPerPage = settings.numberOfPhotoPerPage,
+                numberOfPage = pageNumber).toFlowable()
+                .map { response -> response.photosInfo }
+                .flatMapIterable { photosInfo -> photosInfo.photos }
+                .map { photo -> photo.id }
+                .flatMap { photoId ->
+                    flickrApi.getImageSizes(photoId).toFlowable()
+                            .map { response -> response.sizeInfo }
+                            .map { sizeInfo -> sizeInfo.photoSizes }
+                            .map { sizes -> FlickrImageUrlRetriever(sizes, settings) }
+                            .map { urlRetriever -> FlickrPhoto(urlRetriever) }
+                }.toList()
+    }
+}
--- app/src/main/java/peter/staranchuk/fkickrclientapp/network/sources_settings/FlickrSettings.kt
+++ app/src/main/java/peter/staranchuk/fkickrclientapp/network/sources_settings/FlickrSettings.kt
@@ -3,4 +3,6 @@ package peter.staranchuk.fkickrclientapp.network.sources_settings
 class FlickrSettings {
     val apiKey = "ead31410e5496453f6bc7c25c05d2ea7"
     val baseUrl = "https://api.flickr.com/"
+    val numberOfPhotoPerPage = 20
+    val photoSizeMedium = "Medium"
 }
