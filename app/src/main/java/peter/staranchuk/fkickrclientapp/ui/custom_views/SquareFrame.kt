acdefaf94004a39a561311cd9daa6edc1c1de3ff
peterstaranchuk <petr.staranchuk@gmail.com>
2018-09-22 15:34:16 +0300

    Add square frame custom view to fit photo in square format (user can always see full version of photo in full screen mode)
    Add item photo (for photos grid)
    
    Change app default colors

---
+++ app/src/main/java/peter/staranchuk/fkickrclientapp/ui/custom_views/SquareFrame.kt
@@ -0,0 +1,18 @@
+package peter.staranchuk.fkickrclientapp.ui.custom_views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class SquareFrame : FrameLayout {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+
+    public override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int)
+            = super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+
+}
