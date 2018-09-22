eef1c972aa40cbba22204fb3009c1b218890a4fa
peterstaranchuk <petr.staranchuk@gmail.com>
2018-09-22 17:01:49 +0300

    Add main screen tests
    Add kotlin Extention for image loading

---
+++ app/src/main/java/peter/staranchuk/fkickrclientapp/Extentions.kt
@@ -0,0 +1,8 @@
+package peter.staranchuk.fkickrclientapp
+
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+
+fun ImageView.setImageFromUrl(url : String) {
+    Picasso.get().load(url).into(this)
+}
--- app/src/test/java/peter/staranchuk/fkickrclientapp/MainScreenTests.kt
+++ app/src/test/java/peter/staranchuk/fkickrclientapp/MainScreenTests.kt
@@ -1,6 +1,7 @@
 package peter.staranchuk.fkickrclientapp
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
+import io.reactivex.Single
 import org.hamcrest.CoreMatchers
 import org.junit.Assert
 import org.junit.Before
@@ -9,7 +10,10 @@ import org.junit.Test
 import org.junit.rules.TestRule
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
+import peter.staranchuk.fkickrclientapp.model.objects.FlickrImageUrlRetriever
+import peter.staranchuk.fkickrclientapp.model.objects.FlickrPhoto
 import peter.staranchuk.fkickrclientapp.model.repositories.FlickrPhotosRepository
 import peter.staranchuk.fkickrclientapp.network.FlickrApi
 import peter.staranchuk.fkickrclientapp.network.sources_settings.FlickrSettings
@@ -26,20 +30,39 @@ class MainScreenTests {
     @Mock
     lateinit var settings : FlickrSettings
 
+    @Mock
+    lateinit var flickrRepository : FlickrPhotosRepository
+
     lateinit var viewModelMain : ViewModelMain
 
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
 
-        viewModelMain = Mockito.spy(ViewModelMain(FlickrPhotosRepository(flickrApi, settings)))
+        viewModelMain = Mockito.spy(ViewModelMain(flickrRepository))
     }
 
     @Test
     fun should_show_progress_bar_when_new_images_loading() {
+        `when`(flickrRepository.getPhotos(1)).thenReturn(Single.create {emitter ->
+            val photo = FlickrPhoto(FlickrImageUrlRetriever(arrayListOf(), settings))
+            emitter.onSuccess(arrayListOf(photo))
+        })
+
         viewModelMain.loadNextPage()
-        Assert.assertThat(viewModelMain.isNewImagesLoading.get(), CoreMatchers.equalTo(true))
+
+        Assert.assertThat(viewModelMain.isNewImagesLoading.get(), CoreMatchers.equalTo(false))
     }
 
+    @Test
+    fun should_load_one_element_from_repository() {
+        `when`(flickrRepository.getPhotos(1)).thenReturn(Single.create {emitter ->
+            val photo = FlickrPhoto(FlickrImageUrlRetriever(arrayListOf(), settings))
+            emitter.onSuccess(arrayListOf(photo))
+        })
 
+        viewModelMain.loadNextPage()
+
+        Assert.assertThat(viewModelMain.photos.value!!.size, CoreMatchers.equalTo(1))
+    }
 }
