26154eeebd5443f64acfe54d80c420e8ae11bea4
peterstaranchuk <petr.staranchuk@gmail.com>
2018-09-22 17:36:15 +0300

    Move span count to resources
    Add endless scroll and logic to add new photos to grid

--- app/src/main/java/peter/staranchuk/fkickrclientapp/ui/recent_photos/RecentPhotosFragment.kt
+++ app/src/main/java/peter/staranchuk/fkickrclientapp/ui/recent_photos/RecentPhotosFragment.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -37,10 +38,17 @@ class RecentPhotosFragment : Fragment() {
             //TODO implement
         })
 
+        val gridLayoutManager = GridLayoutManager(context, resources.getInteger(R.integer.spanCount))
+
         rvPhotos.apply {
             setHasFixedSize(true)
-            layoutManager = GridLayoutManager(context, 2)
+            layoutManager = gridLayoutManager
             adapter = recentPhotosAdapter
+            addOnScrollListener(object : EndlessRecyclerViewScrollListener(gridLayoutManager) {
+                override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView) {
+                    viewModel.loadNextPage()
+                }
+            })
         }
 
         viewModel.photos.observe(this, Observer {newPhotos->
