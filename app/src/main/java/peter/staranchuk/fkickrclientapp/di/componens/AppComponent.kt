38103ed447efacfe797994811a31313faf18f366
peterstaranchuk <petr.staranchuk@gmail.com>
2018-09-22 15:10:04 +0300

    Add app component

---
+++ app/src/main/java/peter/staranchuk/fkickrclientapp/di/componens/AppComponent.kt
@@ -0,0 +1,27 @@
+package peter.staranchuk.fkickrclientapp.di.componens
+
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+import dagger.android.support.AndroidSupportInjectionModule
+import peter.staranchuk.fkickrclientapp.PhotoFetcherApp
+import peter.staranchuk.fkickrclientapp.di.modules.AppModule
+import peter.staranchuk.fkickrclientapp.di.modules.FlickrModule
+import peter.staranchuk.fkickrclientapp.di.modules.MainActivityModule
+import peter.staranchuk.fkickrclientapp.di.view_model.ViewModelBuilder
+
+@Component(modules = [ViewModelBuilder::class, MainActivityModule::class, FlickrModule::class, AppModule::class, AndroidSupportInjectionModule::class])
+interface AppComponent : AndroidInjector<PhotoFetcherApp> {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun context(context: PhotoFetcherApp): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(app: DaggerApplication)
+
+}
